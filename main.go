59b1190973d0de7812b08b08b46b5b0e9503be26
Rahul <rahul.mutreja@outlook.com>
2025-02-12 02:40:21 +0530

    testing_further

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Create a basic handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Render!")
+	})
+
+	// Construct the address string: 0.0.0.0:PORT
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
+
+	log.Printf("Server starting on %s", addr)
+
+	// Start the server
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+
 	opt, err := redis.ParseURL("redis://red-cuk6fsij1k6c73d4j520:6379")
 	if err != nil {
 		panic(err)
